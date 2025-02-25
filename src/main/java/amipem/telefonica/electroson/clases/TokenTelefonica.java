e1c86acc13df8a8ace9bcefd21432458a0fd0684
jdiazbasgit <git@studiotapal.es>
2025-02-25 11:44:44 +0100

    estructura

--- src/main/java/amipem/telefonica/electroson/clases/TokenTelefonica.java
+++ src/main/java/amipem/telefonica/electroson/clases/TokenTelefonica.java
@@ -1,9 +1,12 @@
 package amipem.telefonica.electroson.clases;
 
+import java.io.Serializable;
+
 import lombok.Data;
 
 @Data
-public class TokenTelefonica {
+
+public class TokenTelefonica implements Serializable {
 
 	private String access_token, token_type, refresh_token, expires_in, scope, id_token;
 }
